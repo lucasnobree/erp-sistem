2085e3be908a301cb43ad8860a761ea331b4f719
lucasnobree <lucasnobreesteves@gmail.com>
2025-09-04 23:07:14 -0300

    Preparando tela de login

--- src/Pages/Events/events.jsx
+++ src/Pages/Events/events.jsx
@@ -1,26 +1,30 @@
 import { Grid, Box } from "@mui/material";
 import { PageHeader } from "../../components/common/PageHeader/PageHeader";
 import { DataCard } from "../../components/common/DataCard/DataCard";
-import { 
-  People, 
-  Assignment, 
-  TrendingUp, 
-  AttachMoney 
+import {
+  People,
+  Assignment,
+  TrendingUp,
+  AttachMoney,
 } from "@mui/icons-material";
 import styles from "./events.module.css";
 import { KanbanBoard } from "../../components/kanban/KanbanBoard/KanbanBoard";
 
 export default function Eventos() {
+  const handleAddEvent = () => {
+    console.log("Adicionar novo item ao Evento");
+    // Implementar lógica de adição
+  };
 
   return (
     <Box className={styles.Container}>
       <PageHeader
         title="Quadro de Atividades"
         subtitle="Visão geral das suas atividades"
-        addButtonText="Novo Widget"
+        onAdd={handleAddEvent}
+        addButtonText="Novo Evento"
       />
       <KanbanBoard />
-     </Box> 
-      
+    </Box>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Login />,
+        // element: <Login />,
       },
       {
         path: "dashboard",
