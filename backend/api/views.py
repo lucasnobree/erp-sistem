dd675ed8f6e800b11517ff4f61afcc56a1df1842
Leandr0Ms <leandromanoel.sf@gmail.com>
2025-09-05 00:42:38 -0300

    Backend start

---
+++ backend/api/views.py
@@ -0,0 +1,18 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        data = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "is_staff": user.is_staff,
+            "is_superuser": user.is_superuser,
+        }
+        return Response(data)
